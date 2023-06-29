11942e3f7a37c412a46b0a6176a06928e69bb0a5
SujayVullupala123 <75144182+SujayVullupala123@users.noreply.github.com>
2023-06-29 18:32:16 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Bro";
+		
+		if(a==true) {
+			System.out.println("This is true");
+		}
+		
+		if(b=='@') {
+			System.out.println("This is true");
+		}
+		
+		if(c== 123) {
+			System.out.println("This is true");
+			
+		}
+		if(d== 3.14) {
+			System.out.println("This is true");
+			
+		}
+		if(e== "Bro") {
+			System.out.println("This is true");
+			
+		}
+		
+
+	}
+
+}
